d0aaa4ee9c81a258b2e6f140c7fbdd220e5cadae
przemek768 <przemek76816@gmail.com>
2021-01-06 15:24:12 +0100

    Update Kody procedur i wyzwalaczy.sql

--- bazy projekt/Kody procedur i wyzwalaczy.sql
+++ bazy projekt/Kody procedur i wyzwalaczy.sql
@@ -1,3 +1,12 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `1`()
+    NO SQL
+SELECT hurtownie.nazwa_h, produkty.nazwa_p FROM hurtownie INNER JOIN produkty WHERE hurtownie.id_h = produkty.id_h$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `1`(`p0` INT(20)) RETURNS text CHARSET utf8mb4 COLLATE utf8mb4_polish_ci
+    NO SQL
 IF (
   SELECT
     COUNT(*)
@@ -8,8 +17,12 @@ IF (
   AND
 	ilosc_p = 0
 ) > 0 THEN RETURN "Brak produktu";ELSE RETURN "produktu sa jeszcze dostepne";
-END IF;
-SELECT hurtownie.nazwa_h, produkty.nazwa_p FROM hurtownie INNER JOIN produkty WHERE hurtownie.id_h = produkty.id_h;
-UPDATE produkty SET produkty.ilosc_p = produkty.ilosc_p - NEW.ilosc
+END IF$$
+DELIMITER ;
+
+CREATE TRIGGER `1` AFTER INSERT ON `sprzedaz`
+ FOR EACH ROW UPDATE produkty SET produkty.ilosc_p = produkty.ilosc_p - NEW.ilosc
 WHERE produkty.id_p = NEW.id_p;
-INSERT INTO przedawnione_oferty VALUES (null, OLD.id_u, OLD.nazwa_o, OLD.id_p);
+
+CREATE TRIGGER `2` BEFORE DELETE ON `oferta`
+ FOR EACH ROW INSERT INTO przedawnione_oferty VALUES (null, OLD.id_u, OLD.nazwa_o, OLD.id_p);
